17dd4a84f5bccab0ae98168ccd8629c36bc627f1
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 02:12:11 +0800

    created Default layout for pages

---
+++ src/layouts/Default/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+class DefaultLayout extends React.Component {
+  constructor(props: React.Component) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Container>
+        <Header />
+        {this.props.children}
+        <Footer />
+      </Container>
+    );
+  }
+}
+
+export default DefaultLayout;
