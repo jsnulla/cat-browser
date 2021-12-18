58884d75eee0614f2adedd86c2fa3f8b8dc3531a
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 03:09:48 +0800

    added bootstrap css import

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { default as DefaultLayout } from './layouts/Default';
