5f21c1985a7a5bdbc27d8f904468d28f4dc93573
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 02:23:20 +0800

    added router and root route code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import { default as DefaultLayout } from './layouts/Default';
 
 class App extends React.Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <DefaultLayout>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </DefaultLayout>
+    );
   }
 }
 
