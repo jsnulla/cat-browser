7a2f99d4875abe47abc89084021ecd4dfaf1fabe
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 02:10:52 +0800

    created base Header and Footer components

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render = () => {
+    return (
+      <div>
+        <h3>Footer</h3>
+      </div>
+    );
+  };
+}
+
+export default Footer;
