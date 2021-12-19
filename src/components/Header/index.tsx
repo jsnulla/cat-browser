1ff3a01820da1b949108f0101592704658915928
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 20:56:12 +0800

    convert components/Header to function component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
 
-class Header extends React.Component {
-  render = () => {
-    return (
-      <Navbar sticky="top">
-        <Container>
-          <Navbar.Brand href="/">Cat Browser</Navbar.Brand>
-        </Container>
-      </Navbar>
-    );
-  };
-}
+const Header = () => {
+  return (
+    <Navbar sticky="top">
+      <Container>
+        <Navbar.Brand href="/">Cat Browser</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
+};
 
 export default Header;
