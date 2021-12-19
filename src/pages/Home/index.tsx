ef7bfe1354a1d6fe8ff1223ac6c5c936e04ef10d
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 23:03:31 +0800

    add code to load breed options

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,33 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import API from '../../api';
 import InputSelect from '../../components/InputSelect';
+import { apiBreedsFetched } from '../../state/actions';
 
 const Home = () => {
   const [inputOptions, setInputOptions] = useState<any[]>([]);
+  const currentBreedOptions = useSelector((state: any) => state.breedOptions);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentBreedOptions.length === 0) {
+      API.getBreeds().then((apiResponse) => {
+        if (apiResponse.data) {
+          const breeds = apiResponse.data.map((breed: any) => {
+            return {
+              value: breed.id,
+              text: breed.name,
+              description: breed.description,
+            };
+          });
+
+          dispatch(apiBreedsFetched(breeds));
+          setInputOptions(breeds);
+        }
+      });
+    }
+  });
 
   return (
     <Row>
