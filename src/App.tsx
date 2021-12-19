022b84711216c2fb57e516ccac8d72118dc3a3f5
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 20:58:33 +0800

    convert App to function component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import { default as DefaultLayout } from './layouts/Default';
 
-class App extends React.Component {
-  render() {
-    return (
-      <DefaultLayout>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </Router>
-      </DefaultLayout>
-    );
-  }
-}
+const App = () => {
+  return (
+    <DefaultLayout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </DefaultLayout>
+  );
+};
 
 export default App;
