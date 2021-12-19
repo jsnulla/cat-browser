ce6ca7498cbb2338a97762b0a526626abc27bb7a
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 19:51:08 +0800

    removed unused import for component: Footer

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from '../../components/Header';
-import Footer from '../../components/Footer';
 
 class Default extends React.Component {
   constructor(props: React.Component) {
