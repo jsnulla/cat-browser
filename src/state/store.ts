c2b74cc6b1dd9bf33aa00a4ee0189cf06cbdae38
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 22:36:48 +0800

    enable redux devtools

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,4 +1,5 @@
 import { createStore } from 'redux';
 import { appReducer } from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
-export const store = createStore(appReducer);
+export const store = createStore(appReducer, composeWithDevTools());
