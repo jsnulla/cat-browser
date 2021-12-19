5eaa98a0421dd7b7094cfd533b5a817cfc0203a1
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 22:52:24 +0800

    convert pages/Home to function component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-
-class Home extends React.Component {
-  render = () => {
-    return (
-      <div>
-        <h3>Home</h3>
-      </div>
-    );
-  };
-}
+const Home = () => {
+  return (
+    <div>
+      <h3>Home</h3>
+    </div>
+  );
+};
 
 export default Home;
