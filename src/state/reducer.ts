da0f61c6fe65d583893aa884a3c48e98aac0ad40
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 22:46:53 +0800

    update store initialState shape

--- src/state/reducer.ts
+++ src/state/reducer.ts
@@ -1,15 +1,24 @@
-interface AppState {
-  foo: string;
-}
-
-const initialState = {
-  foo: 'bar',
-};
-
 interface Action {
   type: string;
   payload: object;
 }
+
+interface AppState {
+  breedOptions: BreedObject[];
+  selectedBreed: BreedObject | null;
+  error: string;
+}
+
+interface BreedObject {
+  id: string;
+  name: string;
+}
+
+const initialState = {
+  breedOptions: [],
+  selectedBreed: null,
+  error: '',
+};
 
 export const appReducer = (state: AppState = initialState, action: Action) => {
   switch (action.type) {
