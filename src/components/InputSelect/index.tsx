3142f823623ce93060393995fcaaae254b6bd753
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-19 20:54:15 +0800

    convert InputSelect to function component

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -13,44 +13,45 @@ interface InputOption {
   text: string;
 }
 
-class InputSelect extends React.Component<Props> {
-  generateOptionElement(option: InputOption, disabled: boolean = false) {
-    return (
-      <option
-        disabled={disabled}
-        key={`opt-key-${option.value}`}
-        value={option.value}
-      >
-        {option.text}
-      </option>
-    );
-  }
+const generateOptionElement = (
+  option: InputOption,
+  disabled: boolean = false
+) => {
+  return (
+    <option
+      disabled={disabled}
+      key={`opt-key-${option.value}`}
+      value={option.value}
+    >
+      {option.text}
+    </option>
+  );
+};
 
-  renderOptions() {
-    return this.props.options.map((option) => {
-      return this.generateOptionElement(option);
-    });
-  }
+const renderOptions = (options: InputOption[]) => {
+  return options.map((option: InputOption) => {
+    return generateOptionElement(option);
+  });
+};
 
-  render() {
-    return (
-      <React.Fragment>
-        <Form.Label htmlFor={this.props.id}>{this.props.label}</Form.Label>
-        <InputGroup>
-          <select name={this.props.id} id={this.props.id} defaultValue={-1}>
-            {this.generateOptionElement(
-              {
-                value: '-1',
-                text: this.props.defaultOptionText,
-              },
-              true
-            )}
-            {this.renderOptions()}
-          </select>
-        </InputGroup>
-      </React.Fragment>
-    );
-  }
-}
+const InputSelect = (props: Props) => {
+  return (
+    <React.Fragment>
+      <Form.Label htmlFor={props.id}>{props.label}</Form.Label>
+      <InputGroup>
+        <select name={props.id} id={props.id} defaultValue={-1}>
+          {generateOptionElement(
+            {
+              value: '-1',
+              text: props.defaultOptionText,
+            },
+            true
+          )}
+          {renderOptions(props.options)}
+        </select>
+      </InputGroup>
+    </React.Fragment>
+  );
+};
 
 export default InputSelect;
