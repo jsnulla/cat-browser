ce9d491996928661cc897696ba502154d85e20bf
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-21 00:00:00 +0800

    create pages/CatInfo

---
+++ src/pages/CatInfo/index.tsx
@@ -0,0 +1,14 @@
+import { useParams } from 'react-router-dom';
+
+const CatInfo = () => {
+  const params = useParams();
+
+  return (
+    <div>
+      CatInfo! :3
+      <br />
+    </div>
+  );
+};
+
+export default CatInfo;
