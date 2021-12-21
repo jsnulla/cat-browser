79986f168927067c6ed24d41772306859d19c7b5
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-21 22:48:29 +0800

    add data fetching logic to pages/CatInfo

--- src/pages/CatInfo/index.tsx
+++ src/pages/CatInfo/index.tsx
@@ -1,7 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import API from '../../api';
 
 const CatInfo = () => {
   const params = useParams();
+  const { imageId } = useParams();
+  const [catData, setCatData] = useState<any>(null);
+  const [fetchingData, setFetchingData] = useState(false);
+
+  useEffect(() => {
+    if (imageId && catData == null) {
+      API.getImage({ image_id: imageId }).then((response) => {
+        setCatData(response.data);
+      });
+    }
+  });
 
   return (
     <div>
