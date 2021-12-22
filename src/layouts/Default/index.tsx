733c2c0642ca0f885fb32a3a6fdc2bf7e180064d
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-22 23:49:28 +0800

    define props interface for layouts/Default

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,7 +1,11 @@
 import { Container } from 'react-bootstrap';
 import Header from '../../components/Header';
 
-const Default = (props: any) => {
+interface Props {
+  children: JSX.Element;
+}
+
+const Default = (props: Props) => {
   return (
     <Container>
       <Header />
