fb1d7b6c2d4f9dfb90e7e98a17b1e40dcf358816
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-22 23:48:28 +0800

    convert layouts/Default to function component

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
 import { Container } from 'react-bootstrap';
 import Header from '../../components/Header';
 
-class Default extends React.Component {
-  constructor(props: React.Component) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <Container>
-        <Header />
-        {this.props.children}
-      </Container>
-    );
-  }
-}
+const Default = (props: any) => {
+  return (
+    <Container>
+      <Header />
+      {props.children}
+    </Container>
+  );
+};
 
 export default Default;
