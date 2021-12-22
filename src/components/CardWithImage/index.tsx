110201d7819cbe5413e7d1db6866cd2ed3b1ac84
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-22 22:56:37 +0800

    create new component CardWithImage

---
+++ src/components/CardWithImage/index.tsx
@@ -0,0 +1,22 @@
+import { Card } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import ImageWithLoadingAnimation from '../ImageWithLoadingAnimation';
+
+interface Props {
+  srcUrl: string;
+  imageId: string;
+  children?: any;
+}
+
+const CardWithImage = (props: Props) => {
+  return (
+    <Card>
+      <Card.Body className="m-0 p-0">
+        <ImageWithLoadingAnimation srcUrl={props.srcUrl} />
+      </Card.Body>
+      <Card.Footer>{props.children}</Card.Footer>
+    </Card>
+  );
+};
+
+export default CardWithImage;
