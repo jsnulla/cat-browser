b17e0645456cc1137d7a8240b95fb38ef1c79471
Jason Jay S. Nulla <jasonnulla@gmail.com>
2021-12-27 02:31:28 +0800

    add Notification component to Default layout

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'react-bootstrap';
 import Header from '../../components/Header';
+import Notification from '../../components/Notification';
 
 interface Props {
   children: JSX.Element;
@@ -9,6 +10,7 @@ const Default = (props: Props) => {
   return (
     <Container>
       <Header />
+      <Notification />
       {props.children}
     </Container>
   );
